34ecb104e2ba8468bd807880b040ffd0e1e2004e
sabarivel <sabarivelgn.23eie@kongu.edu>
2025-06-23 09:14:08 +0530

    DAY-5

---
+++ DAY-5/app/src/component/pages.jsx
@@ -0,0 +1,14 @@
+import Img from  "../assets/react.svg"
+import "./style/pages.css"
+
+function Pages(){
+    return (
+        <>
+            <center>
+                    <img className="img" src={Img}/>
+            </center>
+        </>
+    )
+}
+
+export default Pages;
